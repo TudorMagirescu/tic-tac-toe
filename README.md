805030fa5bd6e2ff99046a4c56be1357c73fa335
TudorMagirescu <tudor.magirescu@gmail.com>
2022-07-10 18:40:51 +0300

    Updated README. Wrote a comment for the Board constructor.

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Tic-Tac-Toe
 
-A simple version of the popular game 'Tic-Tac-Toe'.
+A simple version of the popular game 'Tic-Tac-Toe'. This project is work in progress.
--- src/board.cpp
+++ src/board.cpp
@@ -1,6 +1,7 @@
 #include "board.h"
 
 Board::Board(){
+    //Initialise each of the board's cells with the Player::NA type
     for(int row=0; row<3; row++)
         for(int col=0; col<3; col++)
             board[row][col] = Player::NA;

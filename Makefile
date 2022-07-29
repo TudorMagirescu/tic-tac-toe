1af6fbd075e4cab1f133019a63bd15dcbbf72ffa
TudorMagirescu <tudor.magirescu@gmail.com>
2022-07-29 16:50:07 +0300

    Modified Makefile to include SFML. Rendered a window and displayed a circle in order to test SFML

--- Makefile
+++ Makefile
@@ -1,7 +1,10 @@
+SFML_PATH = ./sfml
+SFML_LIBS = -lsfml-graphics -lsfml-window -lsfml-system
+
 bin/main: obj/main.o obj/board.o obj/game.o
-	g++ obj/main.o obj/board.o obj/game.o -o bin/main
+	g++ obj/main.o obj/board.o obj/game.o -o bin/main -L$(SFML_PATH)/lib $(SFML_LIBS)
 obj/main.o: src/main.cpp
-	g++ -c src/main.cpp -o obj/main.o
+	g++ -c -I$(SFML_PATH)/include src/main.cpp -o obj/main.o
 obj/board.o: src/board.cpp src/board.h
 	g++ -c src/board.cpp -o obj/board.o
 obj/game.o: src/game.cpp src/game.h
--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,29 @@
+#include <SFML/Graphics.hpp>
 #include <iostream>
 #include "game.h"
 
 int main(){
+    
+
+    //Create a sfml window in order to test sfml
+    //Draw and display a white circle on a black background
+    
+    sf::RenderWindow window(sf::VideoMode(800, 600), "SFML works!");
+
+    while(window.isOpen()){
+        sf::Event event;
+        while(window.pollEvent(event)){
+            if(event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear(sf::Color::Black);
+        sf::CircleShape testCircle(200.f);
+        testCircle.setFillColor(sf::Color::White);
+        window.draw(testCircle);
+        window.display();
+    }
+
     
     Game game;
 

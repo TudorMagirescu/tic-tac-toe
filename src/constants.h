e88e35eb0e78c8969796a0f3c34c6163eaa73129
TudorMagirescu <tudor.magirescu@gmail.com>
2022-08-26 13:44:21 +0200

    Better sprites

--- src/constants.h
+++ src/constants.h
@@ -4,3 +4,4 @@ const unsigned int WINDOW_WIDTH = 800;
 const unsigned int WINDOW_HEIGHT = 850;
 const unsigned int BOARD_WIDTH = 800;
 const unsigned int BOARD_HEIGHT = 800;
+const unsigned int BORDER_SIZE = 3;
--- src/graphics.cpp
+++ src/graphics.cpp
@@ -3,8 +3,9 @@
 void Graphics :: drawSpriteFromFileInCell(sf::RenderWindow &gameWindow, int row, int column, std::string filename){
 
     sf::Vector2f position;
-    position.x = BOARD_WIDTH / 8 + column * BOARD_WIDTH / 4;
-    position.y = BOARD_HEIGHT / 8 + row * BOARD_HEIGHT / 4;
+    position.x = BOARD_WIDTH / 8 + column * BOARD_WIDTH / 4 - BORDER_SIZE * column;
+    position.y = BOARD_HEIGHT / 8 + row * BOARD_HEIGHT / 4 - BORDER_SIZE * row;
+    //the borders of adjacent sprites will overlap
 
     sf::Texture texture;
 
